825d3a1ca8d39f4fb8ef32ad94a8a2dcb0698ba0
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2024-03-15 08:56:06 -0500

    setup with styledcomponents

---
+++ my-app/src/app/page.js
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+import styled from "styled-components";
+
+const Root = styled.div`
+  background-color: black;
+  color: white;
+`;
+
+export default function Home() {
+  return <Root>hello world</Root>;
+}
