9880dbb27a13161339f03725a7830157bdaf44b8
Michael Cuccia <michael.cuccia@egplusww.com>
2024-04-01 12:54:01 -0400

    mobile menu

--- my-app/components/NavBar.jsx
+++ my-app/components/NavBar.jsx
@@ -1,38 +1,31 @@
 'use client';
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 import styled from 'styled-components';
 import { CiUser } from "react-icons/ci";
 import { CiBellOn } from "react-icons/ci";
+import { CiMenuBurger } from "react-icons/ci";
 import { mobileBreakpoint } from '../constants';
-
 
 const Root = styled.div`
   background-color: #FFC72C;
   color: black;
 `;
 
-const MainWrapper = styled.div`
+
+const MainWrapperDesktop = styled.div`
   display: flex;
   justify-content: space-between;
   padding: 8px 0;
 
   @media (max-width: ${mobileBreakpoint}) {
-    flex-direction: column;
-    align-items: center;
-    padding: 0;
+    display: none;
   }
 `;
 
 const LeftSideLinks = styled.div`
   display: flex;
   padding-left: 12px;
-
-  @media (max-width: ${mobileBreakpoint}) {
-    padding-left: 0;
-    flex-direction: column;
-    align-items: center;
-  }
 `;
 
 const NavLinks = styled(Link)`
@@ -42,52 +35,94 @@ const NavLinks = styled(Link)`
   color: inherit;
   cursor: pointer;
 
-  @media (max-width: ${mobileBreakpoint}) {
-    padding-right: 0;
-    font-size: 16px;
-  }
-
   &:hover {
     font-style: italic;
     font-weight: 600;
+  }
+
+  @media (max-width: ${mobileBreakpoint}) {
+    padding: 1.75px 0;
   }
 `;
 
 const RightSideUser = styled.div`
   display: flex;
   justify-content: flex-end;
+  align-items: center;
   padding-right: 12px;
-
-  @media (max-width: ${mobileBreakpoint}) {
-    padding-right: 0;
-    flex-direction: column;
-    align-items: center;
-  }
-
 `;
 
 const IconContainer = styled.div`
   display: flex;
 `;
 
+const MainWrapperMobile = styled.div`
+  display: none;
+
+  @media (max-width: ${mobileBreakpoint}) {
+    display: flex;
+    justify-content: center;
+  }
+`;
+
+const MobileMenuButtonContainer = styled.div`
+  padding-top: 8px;
+  padding-bottom: 6px;
+`;
+
+const MobileNavContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
 export default function NavBar() {
+
+  const [mobileNavOpen, setMobileNavOpen] = useState(false);
+
+  const handleMobileNav = () => {
+    setMobileNavOpen(!mobileNavOpen);
+  }
+
   return (
     <Root>
-      <MainWrapper>
-        <LeftSideLinks>
-          <NavLinks href='/'>Tremelo</NavLinks>
-          <NavLinks href='/'>Home</NavLinks>
-          <NavLinks href='/'>Instruments</NavLinks>
-          <NavLinks href='/'>Add Instruments</NavLinks>
-        </LeftSideLinks>
-        <RightSideUser>
-          <NavLinks href='/'>Login or Register</NavLinks>
-          <IconContainer>
-            <div><CiBellOn /></div>
-            <div><CiUser /></div>
-          </IconContainer>
-        </RightSideUser>
-      </MainWrapper>
+      <MainWrapperDesktop>
+          <LeftSideLinks>
+            <NavLinks href='/'>Tremelo</NavLinks>
+            <NavLinks href='/'>Home</NavLinks>
+            <NavLinks href='/'>Instruments</NavLinks>
+            <NavLinks href='/'>Add Instruments</NavLinks>
+          </LeftSideLinks>
+          <RightSideUser>
+            <NavLinks href='/'>Login or Register</NavLinks>
+            <IconContainer>
+              <div><CiBellOn size="24px" /></div>
+              <div><CiUser size="24px" /></div>
+            </IconContainer>
+          </RightSideUser>
+      </MainWrapperDesktop>
+      {/* MOBILE */}
+      <MainWrapperMobile>
+          <MobileNavContainer>
+            <MobileMenuButtonContainer onClick={handleMobileNav}>
+              <CiMenuBurger />
+            </MobileMenuButtonContainer>
+            {
+              mobileNavOpen && 
+              <>
+                <NavLinks href='/'>Tremelo</NavLinks>
+                <NavLinks href='/'>Home</NavLinks>
+                <NavLinks href='/'>Instruments</NavLinks>
+                <NavLinks href='/'>Add Instruments</NavLinks>
+                <NavLinks href='/'>Login or Register</NavLinks>
+                <IconContainer>
+                  <div><CiBellOn size="24px" /></div>
+                  <div><CiUser size="24px" /></div>
+                </IconContainer>
+              </>
+            }
+          </MobileNavContainer>
+      </MainWrapperMobile>
     </Root>
   )
 }
