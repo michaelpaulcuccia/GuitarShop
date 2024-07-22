3554bbdd26e1e2433732f352f47c6c3b45ae7cac
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2024-07-22 12:02:57 -0500

    connected to DB

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -2,11 +2,14 @@
 import React from "react";
 import styled from "styled-components";
 
-const Root = styled.div`
-`;
+const Root = styled.div``;
 
-export default function Home() {
-  return <Root>
-    <h1>home</h1>
-    </Root>;
-}
+const Home = async () => {
+  return (
+    <Root>
+      <h1>home</h1>
+    </Root>
+  );
+};
+
+export default Home;
